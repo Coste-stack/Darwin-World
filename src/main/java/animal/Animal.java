d36802b74221459928b471a3d064c826832640b0
Coste-stack <p.hajdus1122@gmail.com>
2025-01-24 21:55:38 +0100

    feat: add genotype to animal - determines animal movement directions

--- src/main/java/animal/Animal.java
+++ src/main/java/animal/Animal.java
@@ -5,13 +5,18 @@ import area.Point;
 import javafx.scene.paint.Color;
 import utils.Random;
 
+import java.util.Arrays;
+
 public class Animal {
     private AnimalView animalView; // Visual representation of the animal
     private Point position; // Grid position of the animal
     private Direction direction; // Direction facing the front of the animal
+    private boolean isAlive;
     private int age;
     private static final int MIN_AGE_TO_SKIP_TURN = 10;
-
+    private final int GENOTYPE_SIZE = Direction.values().length;
+    private final Direction[] genotypeList;
+    private int currGenotypeIndex;
     private int energy;
     private static final int DEFAULT_ENERGY = 100;
     private static final int MAX_ENERGY = 100;
@@ -22,16 +27,24 @@ public class Animal {
     private static final int MIN_ENERGY_CONSUMPTION = 5;
     private static final int ENERGY_FOOD_GAIN = 50;
 
-    private boolean isAlive;
-
     public Animal(Point position, float radius) {
         this.animalView = new AnimalView(this, Color.RED, radius);
         this.position = position;
         this.direction = Direction.getRandomDirection();
+        this.isAlive = true;
+        this.age = 0;
         this.energy = DEFAULT_ENERGY;
         this.energyConsumption = DEFAULT_ENERGY_CONSUMPTION;
-        isAlive = true;
-        age = 0;
+        this.genotypeList = generateGenotype();
+        this.currGenotypeIndex = 0;
+    }
+
+    private Direction[] generateGenotype() {
+        Direction[] genotype = new Direction[GENOTYPE_SIZE];
+        for (int i = 0; i < GENOTYPE_SIZE; i++) {
+            genotype[i] = Direction.values()[Random.getRandom(0, Direction.values().length-1)];
+        }
+        return genotype;
     }
 
     private int getSkipTurnChance () {
@@ -40,7 +53,18 @@ public class Animal {
 
     public void move() {
         if (!(age > MIN_AGE_TO_SKIP_TURN && Random.getRandom(0, 100) < getSkipTurnChance())) {
+            // Get direction from genotypes
+            this.direction = this.genotypeList[this.currGenotypeIndex];
+
+            // Set new animal position
+            this.position.setX(this.position.getX() + direction.getDeltaX());
             this.position.setY(this.position.getY() + direction.getDeltaY());
+
+            // Increment genotype index and check if inbounds of list
+            this.currGenotypeIndex++;
+            if (this.currGenotypeIndex >= this.genotypeList.length) {
+                this.currGenotypeIndex = 0;
+            }
         }
         this.subtractEnergy(energyConsumption);
         this.age++;
@@ -142,6 +166,6 @@ public class Animal {
 
     @Override
     public String toString() {
-        return "Animal: " + this.position.toString() + "; energy: " + this.energy + " consumption: " + this.energyConsumption + " %: " + this.getEnergyPercentage();
+        return "Animal: " + this.position.toString() + "; energy: " + this.energy + " consumption: " + this.energyConsumption + " %: " + this.getEnergyPercentage() + "; genotype: " + Arrays.toString(this.genotypeList);
     }
 }
