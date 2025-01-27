227477c951bd06e11f555642344654ebb293553a
Coste-stack <p.hajdus1122@gmail.com>
2025-01-27 03:11:52 +0100

    refactor: move simulation details from main to separate class

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,14 +1,5 @@
 package application;
 
-import animal.AnimalHandler;
-import area.*;
-import area.pole.NorthPole;
-import area.pole.SouthPole;
-import board.Board;
-import board.BoardView;
-import chart.Plot;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import utils.ConfigHandler;
 
 import javafx.scene.layout.*;
@@ -17,10 +8,6 @@ import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.animation.Timeline;
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.util.Duration;
 
 public class Main extends javafx.application.Application {
     public static void main(String[] args) {
@@ -63,63 +50,9 @@ public class Main extends javafx.application.Application {
                     return;
                 }
 
-
-                Board board = new Board();
-                board.addPlot(new Plot());
-
-                // Create the area.pole.Pole at the top and bottom
-                Area NorthPole = new NorthPole(null, null).getArea();
-                Area SouthPole = new SouthPole(null, null).getArea();
-                // Create the area.Plains centered in the grid
-                Area plains = new Plains(null, null).getArea();
-
-                // Add areas to board matrix
-                board.addArea(NorthPole);
-                board.addArea(SouthPole);
-                board.addArea(plains);
-                // Determine preferred food tiles on board
-                board.setFoodPreferredTiles();
-
-                // Create grid visualization in window
-                BoardView boardView = new BoardView(board,
-                        ConfigHandler.getInstance().getConfigValue("SCREEN_WIDTH") / (float) 1.75,
-                        ConfigHandler.getInstance().getConfigValue("SCREEN_HEIGHT") - 20
-                );
-                StackPane boardContainer = boardView.createBoard();
-
-                // Create plot visualization - if plot exists
-                StackPane plotContainer;
-                if (board.getPlot() != null) {
-                    plotContainer = board.getPlot().getLineChart();
-                } else {
-                    plotContainer = new StackPane();
-                }
-
-                // Create a scene
-                HBox root = new HBox();
-                root.getChildren().addAll(plotContainer, boardContainer);
-
-                // Make both components scalable
-                HBox.setHgrow(plotContainer, Priority.ALWAYS);
-                HBox.setHgrow(boardContainer, Priority.ALWAYS);
-
-                // Create animals
-                AnimalHandler animalHandler = new AnimalHandler(board, boardView);
-                for (int i = 0; i < 50; i++) {
-                    animalHandler.createAnimal(new Point());
-                }
-
-                // Run the simulation
-                board.setFoodRandomly();
-                animalHandler.runTurn();
-                boardView.refreshBoard();
-                Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), ev -> {
-                    board.setFoodRandomly();
-                    animalHandler.runTurn();
-                    boardView.refreshBoard();
-                }));
-                timeline.setCycleCount(Animation.INDEFINITE);
-                timeline.play();
+                Simulation simulation = new Simulation();
+                StackPane root = simulation.getView();
+                simulation.run();
 
                 Scene mainScene = new Scene(root);
                 primaryStage.setScene(mainScene);
---
+++ src/main/java/application/Simulation.java
@@ -0,0 +1,98 @@
+package application;
+
+import animal.AnimalHandler;
+import area.Area;
+import area.Plains;
+import area.Point;
+import area.pole.NorthPole;
+import area.pole.SouthPole;
+import board.Board;
+import board.BoardView;
+import chart.Plot;
+import utils.ConfigHandler;
+
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.animation.Timeline;
+import javafx.animation.Animation;
+import javafx.util.Duration;
+import javafx.animation.KeyFrame;
+
+public class Simulation {
+    private Board board;
+    private BoardView boardView;
+    private AnimalHandler animalHandler;
+    private final StackPane simulationView;
+
+    public Simulation() {
+        this.simulationView = this.createView();
+    }
+
+    public StackPane getView() {
+        return this.simulationView;
+    }
+
+    private StackPane createView() {
+        board = new Board();
+        board.addPlot(new Plot());
+
+        // Create the area.pole.Pole at the top and bottom
+        Area NorthPole = new NorthPole(null, null).getArea();
+        Area SouthPole = new SouthPole(null, null).getArea();
+        // Create the area.Plains centered in the grid
+        Area plains = new Plains(null, null).getArea();
+
+        // Add areas to board matrix
+        board.addArea(NorthPole);
+        board.addArea(SouthPole);
+        board.addArea(plains);
+        // Determine preferred food tiles on board
+        board.setFoodPreferredTiles();
+
+        // Create grid visualization in window
+        boardView = new BoardView(board,
+                ConfigHandler.getInstance().getConfigValue("SCREEN_WIDTH") / (float) 1.75,
+                ConfigHandler.getInstance().getConfigValue("SCREEN_HEIGHT") - 20
+        );
+        StackPane boardContainer = boardView.createBoard();
+
+        // Create plot visualization - if plot exists
+        StackPane plotContainer;
+        if (board.getPlot() != null) {
+            plotContainer = board.getPlot().getLineChart();
+        } else {
+            plotContainer = new StackPane();
+        }
+
+        // Create a scene
+        HBox root = new HBox();
+        root.getChildren().addAll(plotContainer, boardContainer);
+
+        // Make both components scalable
+        HBox.setHgrow(plotContainer, Priority.ALWAYS);
+        HBox.setHgrow(boardContainer, Priority.ALWAYS);
+
+        // Create animals
+        animalHandler = new AnimalHandler(board, boardView);
+        for (int i = 0; i < 50; i++) {
+            animalHandler.createAnimal(new Point());
+        }
+
+        return new StackPane(root);
+    }
+
+    public void run() {
+        board.setFoodRandomly();
+        animalHandler.runTurn();
+        boardView.refreshBoard();
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), ev -> {
+            board.setFoodRandomly();
+            animalHandler.runTurn();
+            boardView.refreshBoard();
+        }));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+
+}
